397e95cf8e95f52e086298e9857886848419df3b
imokoi <tiantengfei@outlook.com>
2022-04-11 20:35:08 +0900

    Update NFISHToken.sol

--- contracts/NFISHToken.sol
+++ contracts/NFISHToken.sol
@@ -3,95 +3,46 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract NecoFishingToken is ERC20("NecoFishing", "NFISH"), Ownable {
-    bool public transferLocked = true;
+    using SafeMath for uint;
 
-    mapping (address=>bool) public minters;
     mapping (address=>bool) public burners;
 
-    // time lock for operations.
-    mapping (string=>uint) public operationUnlockTime;
-    string private MINT = "mint";
-    string private TRANSFER = "transfer";
-    bool public mintOperationForOwnerLocked = false;
-    bool public lockTransferOperationForOwnerLocked = false;
+    uint public maxSupply = 100000000 * 1e18;
+    uint public taxRate = 0;
+    address public taxRecipient;
+    mapping(address => bool) public taxTransferBlacklist;
+    mapping(address => bool) public taxTransferFromBlacklist;
 
-    address public mintTo = address(0);
-    uint public mintAmount = 0;
-    uint public unlockDuration = 2 days;
+    address public contractManager;
 
-    event AddMinter(address indexed account);
-    event RemoveMinter(address indexed account);
-    event AddBurner(address indexed account);
-    event RemoveBurner(address indexed account);
-    event TransferUnlocked(bool result);
-
-    /**
-        Minter operations
-     */
-    function addMinter(address account) external onlyOwner {
-        require(account != address(0), "You can not add address 0");
-        minters[account] = true;
-        emit AddMinter(account);
+    constructor() {
+        taxRecipient = owner();
+        contractManager = owner();
+        _mint(owner(), maxSupply);
     }
 
-    function removeMinter(address account) external onlyOwner {
-        minters[account] = false;
-        emit RemoveMinter(account);
-    }
-
-    function addBurner(address account) external onlyOwner {
+    function addBurner(address account) external onlyManager {
         require(account != address(0), "You can not add address 0");
         burners[account] = true;
-        emit AddBurner(account);
     }
 
-    function removeBurner(address account) external onlyOwner {
+    function removeBurner(address account) external onlyManager {
         burners[account] = false;
-        emit RemoveBurner(account);
     }
 
-    /**
-        Mint operations
-     */
-
-    //  owner can not mint NCFH token when mintOperationForOwnerLocked = true.
-    // so owner should use time lock function to mint NCFH tokens.
-    function mint(address to, uint amount) external onlyMinter {
-        if (mintOperationForOwnerLocked) {
-            require(msg.sender != owner(), "Owner can not mint token for now.");
-        }
-        _mint(to, amount);
+    function changeTaxRate(uint newRate) external onlyManager {
+        require(newRate <= 50, "tax rate is so high.");
+        taxRate = newRate;
     }
 
-    // pend mint operation for owner
-    function goingToMint(address to, uint amount) external onlyOwner {
-        require(to != address(0), "Can not mint token to zero address!");
-        mintTo = to;
-        mintAmount = amount;
-        operationUnlockTime[MINT] = block.timestamp + unlockDuration;
+    function changeTaxRecipient(address newAddress) external onlyManager {
+        require(newAddress != address(0), "can not set 0 address.");
+        taxRecipient = newAddress;
     }
 
-    // release mint operation for owner.
-    function releaseMint() external onlyOwner {
-        require(mintAmount > 0, "mint amount can not be 0");
-        require(operationUnlockTime[MINT] != 0, "Bad operationUnlockTime");
-        require(block.timestamp >= operationUnlockTime[MINT], "Mint operation is locked.");
-        _mint(mintTo, mintAmount);
-        mintTo = address(0);
-        mintAmount = 0;
-        operationUnlockTime[MINT] = 0;
-    }
-
-    function changeUnlockTimeDuration(uint newDuration) external onlyOwner {
-        require(newDuration >= 1 days, "Can not change duration to 0");
-        unlockDuration = newDuration;
-    }
-
-    /**
-        Burn operations
-     */
     function burn(uint amount) external returns(bool) {
         require(amount > 0, "can not burn 0 token");
         require(balanceOf(msg.sender) >= amount);
@@ -105,60 +56,43 @@ contract NecoFishingToken is ERC20("NecoFishing", "NFISH"), Ownable {
         return true;
     }
 
-    /**
-        Transfer operations
-     */
     function transfer(address recipient, uint amount) public override returns(bool) {
-        require(msg.sender == owner() || !transferLocked, "Bad Transfer");
         require(balanceOf(msg.sender) >= amount, "insufficient balance.");
-        return super.transfer(recipient, amount);
+        // Tax
+        uint256 taxAmount = 0;
+        if (taxTransferBlacklist[msg.sender]) {
+            taxAmount = amount.mul(taxRate).div(100);
+        }
+        uint256 transferAmount = amount.sub(taxAmount);
+        super.transfer(recipient, transferAmount);
+        if (taxAmount != 0) {
+            super.transfer(taxRecipient, taxAmount);
+        }
+        return true;
     }
 
     function transferFrom(address sender, address recipient, uint amount) public override returns (bool) {
-        require(sender == owner() || !transferLocked, "Bad transferFrom");
         require(balanceOf(sender) >= amount, "insufficient balance.");
-        return super.transferFrom(sender, recipient, amount);
-    }
-
-    function unlockTransfer() external onlyOwner {
-        transferLocked = false;
-        emit TransferUnlocked(transferLocked);
-    }
-
-    // if owner want to lock transfer function. should wait 2 days.
-    function goingToLocakTransferFunction() external onlyOwner {
-        require(!transferLocked, "Transfer function has been locked.");
-        operationUnlockTime[TRANSFER] = block.timestamp + unlockDuration;
-    }
-
-    function releaseTransferLock() external onlyOwner {
-        if(lockTransferOperationForOwnerLocked) {
-            require(block.timestamp >= operationUnlockTime[TRANSFER], "can not lock transfer function.");
+        // For tax
+        uint256 taxAmount = 0;
+        if (taxTransferFromBlacklist[recipient]) {
+            taxAmount = amount.mul(taxRate).div(100);
         }
-        transferLocked = true;
-    }
-
-    /**
-        Lock operations
-     */
-    function enableMintOperationForOwnerLocked() public onlyOwner {
-        mintOperationForOwnerLocked = true;
-    }
-
-    function enableLockTransferOperationForOwnerLocked() public onlyOwner {
-        lockTransferOperationForOwnerLocked = true;
-    }
-
-    /**
-        modifiers
-     */
-    modifier onlyMinter() {
-        require(minters[msg.sender], "Restricted to minters.");
-        _;
+        uint256 transferAmount = amount.sub(taxAmount);
+        super.transferFrom(sender, recipient, transferAmount);
+        if (taxAmount != 0) {
+            super.transferFrom(sender, taxRecipient, taxAmount);
+        }
+        return true;
     }
 
     modifier onlyBurner() {
         require(burners[msg.sender], "Restricted to burners.");
+        _;
+    }
+
+    modifier onlyManager() {
+        require(msg.sender == contractManager, "restrict for contract manager");
         _;
     }
 }
